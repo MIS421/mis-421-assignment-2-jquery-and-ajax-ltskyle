df34d113c883a7ba5727fc7341e6fcf86b5f9a33
Kyle <klknudson@crimson.ua.edu>
2023-09-26 15:52:13 -0500

    some done

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var len;
 var results = '';
 
 function apiSearch() {
+    results = ''
   var params = {
     "q": $("#query").val(),
     "count": "50",
@@ -10,12 +11,12 @@ function apiSearch() {
   };
 
   $.ajax({
-      url: 'my-api-url' + $.param(params),
+      url: 'https://api.bing.microsoft.com//v7.0/search?' + $.param(params),
       beforeSend: function (xhrObj) {
-        xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "my-api-key");
+          xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "c6db692b60d3456aa6135e0bb7a0f4e8");
       },
       type: "GET",
-    })
+  })
     .done(function (data) {
       len = data.webPages.value.length;
       for (i = 0; i < len; i++) {
@@ -28,4 +29,19 @@ function apiSearch() {
     .fail(function () {
       alert("error");
     });
+}
+
+function handleSearchClick() {
+    updateSearchVisibility()
+    apiSearch()
+    updateSearchVisibility()
+}
+
+function updateSearchVisibility() {
+    let visibilityCheck = document.getElementById('searchResults').style.visibility
+    if (visibilityCheck == 'hidden') {
+        document.getElementById('searchResults').style.visibility = 'visible'
+    } else {
+        document.getElementById('searchResults').style.visibility = 'hidden'
+    }
 }
